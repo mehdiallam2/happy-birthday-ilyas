804b0a3903085ec95ffa2e347799b854dbea7391
Mahdi Allam <mahdiallam@Mahdis-MacBook-Pro.local>
2024-03-07 21:24:01 +0100

    fix: birthday day

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import NotFound from "./pages/NotFound";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home name="Ilyas" month={6} day={5} />} />
+      <Route path="/" element={<Home name="Ilyas" month={6} day={30} />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
